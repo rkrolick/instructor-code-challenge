3bc0cd55c0027bd8cb402153d5f3411de6ba2a13
Ryan Krolick <rkrolick@gmail.com>
2016-04-21 14:07:14 -0400

    added .gitignore

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -2,34 +2,31 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
-var bodyParser = express.bodyParser();
+var bodyParser = require('body-parser');
 
-
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use('/', express.static(path.join(__dirname, 'public'));
-
-app.get('/favorites', function(req, res){
-  var data = fs.readFileSync('./data.json');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
-});
-
-app.get('favorites', function(req, res){
-  if(!req.body.name || !req.body.oid){
-    res.send("Error");
-    return
-  }
-
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
-});
-
+app.use('/', express.static(path.join(__dirname, '/public')));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+//
+// app.get('/favorites', function(req, res){
+//   var data = fs.readFileSync('./data.json');
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// });
+//
+// app.get('favorites', function(req, res){
+//   if(!req.body.name || !req.body.oid){
+//     res.send("Error");
+//     return
+//   }
+//
+//   var data = JSON.parse(fs.readFileSync('./data.json'));
+//   data.push(req.body);
+//   fs.writeFile('./data.json', JSON.stringify(data));
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// });
+//
 app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
