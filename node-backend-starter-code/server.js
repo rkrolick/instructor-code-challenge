acfe50425e6e4ef7d5f59d3b444f28e70f0a0320
Ryan Krolick <rkrolick@gmail.com>
2016-04-21 14:13:41 -0400

    git status

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+<<<<<<< HEAD
 var bodyParser = require('body-parser');
 
 app.use('/', express.static(path.join(__dirname, '/public')));
@@ -28,5 +29,33 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 // });
 //
 app.listen(3000, function(){
+=======
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.join(__dirname, 'public'));
+
+app.get('/favorites', function(req, res){
+  var data = fs.readFileSync('./data.json');
+  res.setHeader('Content-Type', 'application/json');
+  res.send(data);
+;
+
+app.get('favorites', function(req, res){
+  if(!req.body.name || !req.body.oid){
+    res.send("Error");
+    return
+  
+  var data = JSON.parse(fs.readFileSync('./data.json'));
+  data.push(req.body);
+  fs.writeFile('./data.json', JSON.stringify(data));
+  res.setHeader('Content-Type', 'application/json');
+  res.send(data);
+});
+
+app.list(3000, function(){
+>>>>>>> parent of 5f6b48d... fixed some errors
   console.log("Listening on port 3000");
 });
