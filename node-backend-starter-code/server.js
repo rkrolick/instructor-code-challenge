e6fd2073ed9b31fa3356315760c8f2d3009b9aa3
Ryan Krolick <rkrolick@gmail.com>
2016-04-21 14:14:06 -0400

    code corrections

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -2,52 +2,27 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
-<<<<<<< HEAD
 var bodyParser = require('body-parser');
 
-app.use('/', express.static(path.join(__dirname, '/public')));
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-//
-// app.get('/favorites', function(req, res){
-//   var data = fs.readFileSync('./data.json');
-//   res.setHeader('Content-Type', 'application/json');
-//   res.send(data);
-// });
-//
-// app.get('favorites', function(req, res){
-//   if(!req.body.name || !req.body.oid){
-//     res.send("Error");
-//     return
-//   }
-//
-//   var data = JSON.parse(fs.readFileSync('./data.json'));
-//   data.push(req.body);
-//   fs.writeFile('./data.json', JSON.stringify(data));
-//   res.setHeader('Content-Type', 'application/json');
-//   res.send(data);
-// });
-//
-app.listen(3000, function(){
-=======
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+
 
 app.get('/favorites', function(req, res){
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+});
 
 app.get('favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error");
     return
-  
+  }
+
   var data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
   fs.writeFile('./data.json', JSON.stringify(data));
@@ -55,7 +30,6 @@ app.get('favorites', function(req, res){
   res.send(data);
 });
 
-app.list(3000, function(){
->>>>>>> parent of 5f6b48d... fixed some errors
+app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
